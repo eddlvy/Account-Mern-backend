881d93a13fa937539f648a6454b53b1608dd1c21
Eduardo Levy <elevypo@gmail.com>
2024-06-16 18:23:15 +0300

    all cors commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://accounteddapp.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE, OPTIONS");
 
