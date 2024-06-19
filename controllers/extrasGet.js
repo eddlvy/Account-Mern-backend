00f8c8a0930505083caef92e853f4a64999e6f5c
Eduardo Levy <elevypo@gmail.com>
2024-06-19 17:29:42 +0300

    Extras Update

--- controllers/extrasGet.js
+++ controllers/extrasGet.js
@@ -5,7 +5,7 @@ const ExtrasModel = require('../models/extras');
 const extrasGet = async (req, res) => {
   try {
     const mes = req.params.mes
-    const horas = await ExtrasModel.find({ "mes": mes }, { concepto: 1, cantidad: 1 }, { _id: 0, mes: 0 });
+    const horas = await ExtrasModel.find({ "mes": mes }, { _id: 0 });
     return res.send(horas)
 
   } catch (error) {
