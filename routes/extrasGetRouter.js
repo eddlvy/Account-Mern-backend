91daf015fe37f0f5ba8a0d02f86e4f6c18ba03f6
Eduardo Levy <elevypo@gmail.com>
2024-06-19 17:24:43 +0300

    Extras Update

--- routes/extrasGetRouter.js
+++ routes/extrasGetRouter.js
@@ -3,6 +3,6 @@ const extrasGetRouter = express.Router();
 const extrasGet = require('../controllers/extrasGet');
 
 
-historiaMesRouter.route('/:mes').get(extrasGet);
+extrasGetRouter.route('/:mes').get(extrasGet);
 
 module.exports = extrasGetRouter;
